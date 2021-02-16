ba24e0390bc728efbb40a10b0e4f6dee0b49d185
PrynsTag <201812311@fit.edu.ph>
2021-02-16 23:28:15 +0800

    5th iteration, added menu, return to menu and formatting.

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,67 +1,89 @@
 open class CoffeeMachine(
-    open var amountWater: Int = 400, open var amountMilk: Int = 540, open var amountBeans: Int = 120,
-    open var disposableCups: Int = 9, open var amountMoney: Int = 550
+    open var amountWater: Int = 400,
+    open var amountMilk: Int = 540,
+    open var amountBeans: Int = 120,
+    open var disposableCups: Int = 9,
+    open var amountMoney: Int = 550,
 ) {
 
     data class Espresso(
-        override var amountWater: Int = 250, override var amountBeans: Int = 16,
-        override var disposableCups: Int = 1, override var amountMoney: Int = 4
+        override var amountWater: Int = 250,
+        override var amountBeans: Int = 16,
+        override var disposableCups: Int = 1,
+        override var amountMoney: Int = 4,
     ) : CoffeeMachine()
 
     data class Latte(
-        override var amountWater: Int = 350, override var amountBeans: Int = 20,
-        override var disposableCups: Int = 1, override var amountMoney: Int = 7,
+        override var amountWater: Int = 350,
+        override var amountBeans: Int = 20,
+        override var disposableCups: Int = 1,
+        override var amountMoney: Int = 7,
         override var amountMilk: Int = 75,
     ) : CoffeeMachine()
 
     data class Cappuccino(
-        override var amountWater: Int = 200, override var amountBeans: Int = 12,
-        override var disposableCups: Int = 1, override var amountMoney: Int = 6,
+        override var amountWater: Int = 200,
+        override var amountBeans: Int = 12,
+        override var disposableCups: Int = 1,
+        override var amountMoney: Int = 6,
         override var amountMilk: Int = 100,
     ) : CoffeeMachine()
 
     override fun toString(): String {
-        return """
-            The coffee machine has:
-            $amountWater of water
-            $amountMilk of milk
-            $amountBeans of coffee beans
-            $disposableCups of disposable cups
-            $amountMoney of money
-        """.trimIndent()
+        return "\nThe coffee machine has:\n" +
+                "$amountWater of water\n" +
+                "$amountMilk of milk\n" +
+                "$amountBeans of coffee beans\n" +
+                "$disposableCups of disposable cups\n" +
+                "$$amountMoney of money\n"
     }
 
     fun startMachine() {
-        println(this)
-        print("\nWrite action (buy, fill, take): ")
-        when (readLine()!!) {
-            "buy" -> buyCoffee()
-            "fill" -> fillMachine()
-            "take" -> takeMoney()
+        while (true) {
+            print("Write action (buy, fill, take, remaining, exit): ")
+            when (readLine()!!) {
+                "buy" -> buyCoffee()
+                "fill" -> fillMachine()
+                "take" -> takeMoney()
+                "remaining" -> println(this)
+                "exit" -> return
+            }
         }
     }
 
     private fun buyCoffee() {
-        print("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino: ")
-        when (readLine()!!.toInt()) {
-            1 -> reduceResource(Espresso())
-            2 -> reduceResource(Latte())
-            3 -> reduceResource(Cappuccino())
+        print("\nWhat do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:")
+        when (readLine()!!) {
+            "1" -> reduceResource(Espresso())
+            "2" -> reduceResource(Latte())
+            "3" -> reduceResource(Cappuccino())
+            "back" -> return
             else -> println("Wrong Input..")
         }
     }
 
     private fun reduceResource(coffee: CoffeeMachine) {
-        println()
+        when {
+            this.amountWater - coffee.amountWater <= 0 -> return println("Sorry not enough water!\n")
+            this.amountBeans - coffee.amountBeans <= 0 -> return println("Sorry not enough beans!\n")
+            this.disposableCups - coffee.disposableCups <= 0 -> return println("Sorry not enough cups!\n")
+            coffee !is Espresso -> {
+                if (this.amountMilk - coffee.amountMilk <= 0)
+                    return println("Sorry not enough milk!")
+                else
+                    this.amountMilk -= coffee.amountMilk
+            }
+        }
+
         this.amountWater -= coffee.amountWater
-        if (coffee !is Espresso) this.amountMilk -= coffee.amountMilk
         this.amountBeans -= coffee.amountBeans
         this.disposableCups -= coffee.disposableCups
         this.amountMoney += coffee.amountMoney
-        println(this)
+        println("I have enough resources, making you a coffee!\n")
     }
 
     private fun fillMachine() {
+        println()
         print("Write how many ml of water do you want to add: ")
         this.amountWater += readLine()!!.toInt()
 
@@ -74,13 +96,12 @@ open class CoffeeMachine(
         print("Write how many disposable cups of coffee do you want to add: ")
         this.disposableCups += readLine()!!.toInt()
         println()
-        print(this)
     }
 
     private fun takeMoney() {
+        println()
         println("I gave you $$amountMoney\n")
         this.amountMoney = 0
-        println(this)
     }
 }
 
